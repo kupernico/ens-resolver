3709390d6ff4cd66dc80318b5e0e993ac9f07dca
kupernico <83714449+kupernico@users.noreply.github.com>
2022-01-16 17:22:07 +0100

    Add Cloudflare provider

--- src/handlers/domain2avatar.ts
+++ src/handlers/domain2avatar.ts
@@ -1,24 +1,24 @@
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
 
 const Domain2Avatar = async request => {
     const domain = request.params.domain
 
-    const avatar = await fetchENSAvatar(domain);
+    const avatar = await fetchENSAvatar(domain)
 
-    const body = JSON.stringify({ domain: domain, avatar: avatar})
+    const body = JSON.stringify({ avatar: avatar})
     const headers = { 'Content-type': 'application/json' }
     return new Response(body, { headers })
   }
   
 export default Domain2Avatar
 
+
 async function fetchENSAvatar(domain: string): Promise<string> {
-  var url = 'YOUR_ETHEREUM_ENDPOINT'
-  
-  const provider = new ethers.providers.JsonRpcProvider(url);
-  const resolver = await provider.getResolver(domain);
-  const result = await resolver.getText("avatar");
+  // Connect to mainnet (homestead)
+  const provider = new ethers.providers.CloudflareProvider( )
 
-  return result;
+  const resolver = await provider.getResolver(domain)
+  const avatar = await resolver.getText("avatar")
+
+  return avatar;
 }
-
